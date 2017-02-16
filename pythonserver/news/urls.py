7e27d83ed944f47e311b8dfee8c6aebb0c776d56
zhouying <786551136@qq.com>
2017-02-16 10:15:19 +0800

    路由跳转

--- pythonserver/news/urls.py
+++ pythonserver/news/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
-from . import views
+from . import cmd
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', cmd.index, name='index'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', cmd.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', cmd.vote, name='vote'),
 ]
--- pythonserver/pythonserver/settings.py
+++ pythonserver/pythonserver/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'PASSWORD':'1q2w3e',
         "HOST": '127.0.0.1',
         'PORT': '3306',
+        'CHARSET': 'utf-8',
     }
 }
 
